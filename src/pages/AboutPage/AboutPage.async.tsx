3b45fa024a6c1d1ce1db708005ab3cc7e15329f5
Селиванов Иван <iselivanov@office.local>
2023-10-12 11:16:16 +0300

    initial commit

---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,10 @@
+import { lazy } from "react"
+
+export const AboutPageAsync = lazy(
+  () =>
+    // import('./AboutPage')
+    new Promise((resolve) => {
+      // @ts-ignore
+      setTimeout(() => resolve(import("./AboutPage")), 1500)
+    })
+)
