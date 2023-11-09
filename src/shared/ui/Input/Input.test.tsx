8133bc82858f0efeb3498d6022152dc8406030fd
Mollycodd1e <selik7@mail.ru>
2023-11-09 12:39:07 +0300

    fix: err

--- src/shared/ui/Input/Input.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { Input} from 'shared/ui/Input/Input'
-
-describe('classNames', () => {
-	test('render', () => {
-		render(<Input>TEST</Input>)
-		expect(screen.getByText('TEST')).toBeInTheDocument()
-	})
-})
