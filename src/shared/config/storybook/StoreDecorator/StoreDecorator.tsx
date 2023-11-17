0e0330d6277c7fba82dd5be67f6b4f58405ed1c1
Mollycodd1e <selik7@mail.ru>
2023-11-17 17:07:03 +0300

    fix: build error

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -2,9 +2,8 @@ import { StoryFn } from '@storybook/react'
 import { StoreProvider } from 'app/providers/StoreProvider/StoreProvider'
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema'
 import 'app/styles/index.scss'
-import { DeepPartial } from 'redux'
 
-export const StoreDecorator = (state: DeepPartial<StateSchema>) => (story: () => StoryFn) => {
+export const StoreDecorator = (state: StateSchema) => (story: () => StoryFn) => {
 	return (
 		<StoreProvider initialState={state}>
 			<div>{story()}</div>
